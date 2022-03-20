0f10a674952d2323b13c8631d5e889b92de9611f
LinhLe2307 <lemylinh23071997@gmail.com>
2022-03-20 14:01:52 +0200

    get the current data's endpoint

--- src/main.js
+++ src/main.js
@@ -21,77 +21,83 @@ const API = new FetchWrapper(
   "https://firestore.googleapis.com/v1/projects/programmingjs-90a13/databases/(default)/documents/"
 );
 
-API.get("LinhLe").then((data) => createCard(data.documents));
+API.get("LinhLe").then((data) => fetchCards(data.documents));
 
-const reloadPage = () => {
-  window.location.reload();
-};
-
-const showAlertMessage = () => {
+// to hide the message
+const hideAlertMessage = () => {
   alertMessage.style.visibility = "hidden";
 };
 
+// total calories
 let total = 0;
-const createCard = (items) => {
+
+const fetchCards = (items) => {
   if (items) {
     const displayCard = items.map((item) => {
       if (item.fields) {
-        // console.log(item.fields)
-        const div = document.createElement("div");
-        const div2 = document.createElement("div");
-        const li = document.createElement("li");
-        const h2 = document.createElement("h2");
-        const p = document.createElement("p");
-        const pCarb = document.createElement("p");
-        const pProtein = document.createElement("p");
-        const pFat = document.createElement("p");
-        const deleteBtn = document.createElement("button");
-
-        div.classList.add("item-container");
-        li.classList.add("food-item");
-        deleteBtn.classList.add("delete-button");
-
-        //take the food Endpoint, the endpoint starts at index 59
-        div.setAttribute("data-food-endpoint", item.name.slice(59));
-        div.setAttribute("data-protein", item.fields.protein.integerValue);
-        div.setAttribute("data-carb", item.fields.carb.integerValue);
-        div.setAttribute("data-fat", item.fields.fat.integerValue);
-
-        // this is for each card's calories
-        let cardCalories =
-          +item.fields.protein.integerValue * 4 +
-          +item.fields.carb.integerValue * 4 +
-          +item.fields.fat.integerValue * 9;
-
-        h2.textContent = item.fields.name.stringValue;
-        p.textContent = `${cardCalories} calories`;
-        pCarb.textContent = `Carb: ${item.fields.carb.integerValue}g`;
-        pProtein.textContent = `Protein: ${item.fields.protein.integerValue}g`;
-        pFat.textContent = `Fat: ${item.fields.fat.integerValue}g`;
-
-        // this is for total calories
-        total += cardCalories;
-
-        li.appendChild(h2);
-        li.appendChild(p);
-        li.appendChild(pCarb);
-        li.appendChild(pProtein);
-        li.appendChild(pFat);
-        li.appendChild(deleteBtn);
-        div2.appendChild(li);
-        createImg(item, div2);
-        div.appendChild(div2);
-
-        ul.appendChild(div);
+        createCard(item);
       }
     });
-    totalCalories.innerHTML = `Total calories logged: <span>${total}</span>`;
-    protein.value = "";
-    carb.value = "";
-    fat.value = "";
   }
 };
 
+// for indivudual card
+const createCard = (item) => {
+  const div = document.createElement("div");
+  const div2 = document.createElement("div");
+  const li = document.createElement("li");
+  const h2 = document.createElement("h2");
+  const p = document.createElement("p");
+  const pCarb = document.createElement("p");
+  const pProtein = document.createElement("p");
+  const pFat = document.createElement("p");
+  const deleteBtn = document.createElement("button");
+
+  div.classList.add("item-container");
+  li.classList.add("food-item");
+  deleteBtn.classList.add("delete-button");
+
+  //take the food Endpoint, the endpoint starts at index 59
+  div.setAttribute("data-food-endpoint", item.name.slice(59));
+
+  div.setAttribute("data-protein", item.fields.protein.integerValue);
+  div.setAttribute("data-carb", item.fields.carb.integerValue);
+  div.setAttribute("data-fat", item.fields.fat.integerValue);
+
+  // this is for each card's calories
+  let cardCalories =
+    +item.fields.protein.integerValue * 4 +
+    +item.fields.carb.integerValue * 4 +
+    +item.fields.fat.integerValue * 9;
+
+  h2.textContent = item.fields.name.stringValue;
+  p.textContent = `${cardCalories} calories`;
+  pCarb.textContent = `Carb: ${item.fields.carb.integerValue}g`;
+  pProtein.textContent = `Protein: ${item.fields.protein.integerValue}g`;
+  pFat.textContent = `Fat: ${item.fields.fat.integerValue}g`;
+
+  // this is for total calories
+  total += cardCalories;
+
+  li.appendChild(h2);
+  li.appendChild(p);
+  li.appendChild(pCarb);
+  li.appendChild(pProtein);
+  li.appendChild(pFat);
+  li.appendChild(deleteBtn);
+  div2.appendChild(li);
+  createImg(item, div2);
+  div.appendChild(div2);
+
+  ul.appendChild(div);
+
+  totalCalories.innerHTML = `Total calories logged: <span>${total}</span>`;
+  protein.value = "";
+  carb.value = "";
+  fat.value = "";
+};
+
+// to post the data to API
 const postCardAPI = () => {
   API.post("LinhLe", {
     fields: {
@@ -111,23 +117,40 @@ const postCardAPI = () => {
   });
 };
 
+// to display pie chart, post data and fetch the current item after post using promise. Otherwise, it will run synchronous => no data to fetch
 const submitForm = (e) => {
-  showAlertMessage();
   e.preventDefault();
-  showChart(carb.value, protein.value, fat.value, foodName.value);
+
+  // post the data to API
   postCardAPI();
+
+  // get the endpoint of the last item after posting it to API. Waiting for 100 milisseconds before fetch it => will have the endpoint
+  new Promise((resolve, reject) => {
+    setTimeout(() => resolve(API.get("LinhLe")), 100);
+  }).then((data) => createCard(data.documents[data.documents.length - 1]));
+
+  //to hide the alert message
+  hideAlertMessage();
+
+  // to display the chart to the browser
+  showChart(carb.value, protein.value, fat.value, foodName.value);
+
+  // postCardAPI();
+
+  // to close the add form
   closeForm();
-  // reloadPage();
-  // foodForm.submit();
+
+  // have snackbar
   snackbar.show("Food added successfully");
 };
 
 const showCard = (e) => {
   if (e.target.classList.contains("food-item")) {
-    showAlertMessage();
+    hideAlertMessage();
     const itemContainer = e.target.parentElement.parentElement;
+
+    // display individual card using its food endpoint
     API.get(itemContainer.dataset.foodEndpoint).then((data) => {
-      // console.log(data);
       const currentProtein = data.fields.protein.integerValue;
       const currentCarb = data.fields.carb.integerValue;
       const currentFat = data.fields.fat.integerValue;
@@ -144,10 +167,14 @@ const showChart = (
   currentFat,
   currentFoodName
 ) => {
+  // destroy the id of current chart
   if (currentChart) {
     currentChart.destroy();
   }
+
   displayPieChart.innerHTML = `Food Name: <span>${currentFoodName}</span>`;
+
+  // update the current one
   const ctx = document.getElementById("myChart");
   currentChart = new Chart(ctx, {
     type: "doughnut",
@@ -157,7 +184,6 @@ const showChart = (
         {
           label: "# of Votes",
           data: [currentCarb, currentProtein, currentFat],
-          // backgroundColor: ["#b5838d", "#e5989b", "#ffb4a2"],
           backgroundColor: ["#b388eb", "#f7aef8", "#8093f1"],
           borderColor: ["#826aed", "#fb6376", "#33658a"],
           borderWidth: 1,
@@ -174,10 +200,12 @@ const showChart = (
   });
 };
 
+// display the form when clicking the add button
 const addForm = () => {
   addContent.style.visibility = "visible";
 };
 
+// hide the form when clicking the back button
 const closeForm = () => {
   addContent.style.visibility = "hidden";
 };
@@ -189,10 +217,14 @@ const deleteItem = (e) => {
 
     // to take the div food Endpoint and delete it
     API.delete(wrapper.parentElement.dataset.foodEndpoint, {});
+
+    //update total calories by minusing the calories of the deleted item
     total -=
       +wrapper.parentElement.dataset.protein * 4 +
       +wrapper.parentElement.dataset.carb * 4 +
       +wrapper.parentElement.dataset.fat * 9;
+
+    // display the total
     totalCalories.innerHTML = `Total calories logged: <span>${total}</span>`;
     snackbar.show("Food removed successfully");
   }
